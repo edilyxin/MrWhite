df94204d4bb2b92555a8ac3a4fd684ec16bccd25
roy <topcatv@gmail.com>
2014-04-04 20:30:48 +0800

    add gem source of china mirror

--- rails_evn_installer.sh
+++ rails_evn_installer.sh
@@ -7,6 +7,8 @@ echo -e 'install: --no-rdoc --no-ri\nupdate: --no-rdoc --no-ri' > ~/.gemrc
 source ~/.rvm/scripts/rvm
 rvm requirements
 rvm install 2.0.0 --with-openssl-dir=$HOME/.rvm/usr
+gem sources --remove https://rubygems.org/
+gem sources -a http://ruby.taobao.org/
 rvm use 2.0.0@rails_4_0 --create --default
 gem update --system 2.1.9
 gem install rails --version 4.0.4 --no-ri --no-rdoc
